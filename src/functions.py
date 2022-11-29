f31982c48bdb505f8a67b57f043b50990230cd89
cxnt <pavelbartsits@gmail.com>
2022-11-29 18:21:10 +0400

    skip image with different shape

--- src/functions.py
+++ src/functions.py
@@ -4,30 +4,41 @@ import subprocess
 
 import sly_globals as g
 
-import supervisely_lib as sly
-from supervisely_lib.io.fs import silent_remove
-from supervisely_lib.video_annotation.video_tag_collection import VideoTag, VideoTagCollection
+import supervisely as sly
+from supervisely.io.fs import silent_remove
+from supervisely.video_annotation.video_tag_collection import (
+    VideoTag,
+    VideoTagCollection,
+)
 
 
 def process_video(api, img_dataset, vid_dataset):
-    images_infos = api.image.get_list(img_dataset.id, sort='name')
+    images_infos = api.image.get_list(img_dataset.id, sort="name")
     if len(images_infos) == 0:
-        g.my_app.logger.warn(f'There are no images in {img_dataset.name} dataset')
+        g.my_app.logger.warn(f"There are no images in {img_dataset.name} dataset")
     image_shape = None
+
+    images_ids = []
+    images_paths = []
     for idx, image_info in enumerate(images_infos):
         if idx == 0:
             image_shape = (image_info.width, image_info.height)
+            images_ids.append(image_info.id)
+            images_paths.append(os.path.join(g.work_dir, image_info.name))
+        elif (image_info.width, image_info.height) == image_shape:
+            images_ids.append(image_info.id)
+            images_paths.append(os.path.join(g.work_dir, image_info.name))
         elif (image_info.width, image_info.height) != image_shape:
-            g.my_app.logger.warn(f'Sizes of images in {img_dataset.name} dataset are not the same. Check your input data.')
-            g.my_app.stop()
-            return
+            g.my_app.logger.warn(
+                f"Sizes of images in {img_dataset.name} dataset are not the same. Check your input data."
+            )
+            continue
 
-    images_ids = [image_info.id for image_info in images_infos]
-    images_paths = [os.path.join(g.work_dir, image_info.name) for image_info in images_infos]
     api.image.download_paths(img_dataset.id, images_ids, images_paths)
-
     video_path = os.path.join(g.work_dir, f"{vid_dataset.name}.mp4")
-    video = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), int(g.FRAME_RATE), image_shape)
+    video = cv2.VideoWriter(
+        video_path, cv2.VideoWriter_fourcc(*"mp4v"), int(g.FRAME_RATE), image_shape
+    )
     progress = sly.Progress("Processing video frames:", len(images_infos))
     for img_path in images_paths:
         img = cv2.imread(img_path)
@@ -35,21 +46,35 @@ def process_video(api, img_dataset, vid_dataset):
         silent_remove(img_path)
         progress.iter_done_report()
     video.release()
-    
-    
+
     if os.path.isfile(video_path):
-        converted_path = video_path.replace(f"{vid_dataset.name}.mp4", f"converted_{vid_dataset.name}.mp4")
+        converted_path = video_path.replace(
+            f"{vid_dataset.name}.mp4", f"converted_{vid_dataset.name}.mp4"
+        )
         # ffmpeg -i input.mp4 -c:v libvpx-vp9 -c:a libopus output.webm
-        subprocess.call(['ffmpeg', '-y', '-i', f'{video_path}', '-c:v', 'libx264', '-c:a',
-                         'libopus', f'{converted_path}'])
+        subprocess.call(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                f"{video_path}",
+                "-c:v",
+                "libx264",
+                "-c:a",
+                "libopus",
+                f"{converted_path}",
+            ]
+        )
         os.remove(video_path)
         os.rename(converted_path, video_path)
-    
+
     video_names = [f"{vid_dataset.name}.mp4"]
     video_paths = [video_path]
 
     upl_progress = sly.Progress("Uploading video:", 1)
-    video_info = api.video.upload_paths(vid_dataset.id, names=video_names, paths=video_paths)
+    video_info = api.video.upload_paths(
+        vid_dataset.id, names=video_names, paths=video_paths
+    )
     upl_progress.iter_done_report()
 
     video_info = video_info[0]
@@ -61,7 +86,9 @@ def get_object_name_id_map(anns):
     unique_obj_ids = {}
     for ann in anns:
         for label in ann.labels:
-            filtered_tag_vals = [tag.value for tag in label.tags if tag.name == 'object_id']
+            filtered_tag_vals = [
+                tag.value for tag in label.tags if tag.name == "object_id"
+            ]
             if len(filtered_tag_vals) == 0:
                 continue
             if label.obj_class.name in unique_obj_ids:
@@ -77,7 +104,9 @@ def create_id_to_video_objects_map_from_object_name_ids_map(object_name_ids_map)
     video_objects_map = {}
     for obj_class_name, obj_class_ids in object_name_ids_map.items():
         for obj_class_id in obj_class_ids:
-            video_object = sly.VideoObject(g.project_meta.get_obj_class(obj_class_name), class_id=obj_class_id)
+            video_object = sly.VideoObject(
+                g.project_meta.get_obj_class(obj_class_name), class_id=obj_class_id
+            )
             video_objects_map[obj_class_id] = video_object
     return video_objects_map
 
@@ -89,7 +118,9 @@ def process_annotations(api, meta, img_dataset, video_info, images_ids):
     video_objects_map = None
     object_name_ids_map = get_object_name_id_map(anns)
     if len(object_name_ids_map) > 0:
-        video_objects_map = create_id_to_video_objects_map_from_object_name_ids_map(object_name_ids_map)
+        video_objects_map = create_id_to_video_objects_map_from_object_name_ids_map(
+            object_name_ids_map
+        )
     video_objects_col = []
     video_frames_col = []
     video_tags_col = []
@@ -109,10 +140,14 @@ def process_annotations(api, meta, img_dataset, video_info, images_ids):
                 if tag.name == "object_id":
                     vobj_id = tag.value
             if vobj_id is not None:
-                figure = sly.VideoFigure(video_objects_map[vobj_id], label.geometry, idx, class_id=vobj_id)
+                figure = sly.VideoFigure(
+                    video_objects_map[vobj_id], label.geometry, idx, class_id=vobj_id
+                )
                 figures.append(figure)
             else:
-                video_object = sly.VideoObject(label.obj_class, VideoTagCollection(object_tag_col))
+                video_object = sly.VideoObject(
+                    label.obj_class, VideoTagCollection(object_tag_col)
+                )
                 video_objects_col.append(video_object)
                 figure = sly.VideoFigure(video_object, label.geometry, idx)
                 figures.append(figure)
@@ -128,13 +163,17 @@ def process_annotations(api, meta, img_dataset, video_info, images_ids):
     img_size = anns[0].img_size
 
     if video_objects_map is not None:
-        video_objects_col = sly.VideoObjectCollection(video_objects_col + list(video_objects_map.values()))
+        video_objects_col = sly.VideoObjectCollection(
+            video_objects_col + list(video_objects_map.values())
+        )
     else:
         video_objects_col = sly.VideoObjectCollection(video_objects_col)
 
     video_frames_col = sly.FrameCollection(video_frames_col)
     video_tags_col = VideoTagCollection(video_tags_col)
     upl_progress = sly.Progress("Uploading video annotation:", 1)
-    video_ann = sly.VideoAnnotation(img_size, len(anns), video_objects_col, video_frames_col, video_tags_col)
+    video_ann = sly.VideoAnnotation(
+        img_size, len(anns), video_objects_col, video_frames_col, video_tags_col
+    )
     api.video.annotation.append(video_info.id, video_ann)
     upl_progress.iter_done_report()
--- src/sly_globals.py
+++ src/sly_globals.py
@@ -1,22 +1,30 @@
-import os
 import json
-import supervisely_lib as sly
-from supervisely_lib.io.fs import mkdir
+import os
+
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.io.fs import mkdir
+
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 
 my_app = sly.AppService()
 api: sly.Api = my_app.public_api
 
-TEAM_ID = int(os.environ['context.teamId'])
-WORKSPACE_ID = int(os.environ['context.workspaceId'])
-PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
-TASK_ID = int(os.environ["TASK_ID"])
+TASK_ID = sly.env.task_id()
+TEAM_ID = sly.env.team_id()
+WORKSPACE_ID = sly.env.workspace_id()
+PROJECT_ID = sly.env.project_id()
 
 logger = sly.logger
 
-FRAME_RATE = os.environ['modal.state.frameRate']
-SELECTED_DATASETS = json.loads(os.environ["modal.state.selectedDatasets"].replace("'", '"'))
-ALL_DATASETS = os.getenv("modal.state.allDatasets").lower() in ('true', '1', 't')
+FRAME_RATE = os.environ["modal.state.frameRate"]
+SELECTED_DATASETS = json.loads(
+    os.environ["modal.state.selectedDatasets"].replace("'", '"')
+)
+ALL_DATASETS = os.getenv("modal.state.allDatasets").lower() in ("true", "1", "t")
 if ALL_DATASETS:
     SELECTED_DATASETS = [dataset.name for dataset in api.dataset.get_list(PROJECT_ID)]
 
@@ -24,7 +32,11 @@ project_info = api.project.get_info_by_id(PROJECT_ID)
 if project_info is None:
     raise RuntimeError("Project {!r} not found".format(project_info.name))
 if project_info.type != str(sly.ProjectType.IMAGES):
-    raise TypeError("Project type is {!r}, but have to be {!r}".format(project_info.type, sly.ProjectType.IMAGES))
+    raise TypeError(
+        "Project type is {!r}, but have to be {!r}".format(
+            project_info.type, sly.ProjectType.IMAGES
+        )
+    )
 
 project_meta_json = api.project.get_meta(project_info.id)
 project_meta = sly.ProjectMeta.from_json(project_meta_json)
